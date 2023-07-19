d861da604e8d7b61ce9333ef46168ee643ec09a2
cadelljulienfau <cjulien2019@fau.edu>
2023-07-19 15:28:38 -0400

    Added to signup

--- frontend/SignUpScreen.js
+++ frontend/SignUpScreen.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text, TextInput } from 'react-native';
 import axios from 'axios';
 
 export default function SignUpScreen({ handleBackPress }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post('/signup', {
+        username,
+        password,
+      });
+      console.log(response.data);
+      // Do something with the response if needed
+    } catch (error) {
+      console.error(error);
+      // Handle the error if needed
+    }
+  };
+
   return (
     <View style={styles.container}>
+
+      <Text style={styles.welcome}>Let's get you signed up!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.signupButton} onPress={handleSignUp}>
+        <Text>Sign Up</Text>
+      </TouchableOpacity>
       <TouchableOpacity style={styles.backButton} onPress={handleBackPress}>
         <Text>Back</Text>
       </TouchableOpacity>
-      <Text>Sign Up Screen</Text>
-      {/* Add your sign up screen components here */}
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -31,7 +66,7 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     fontWeight: 'light',
-    marginBottom: 5,
+    marginBottom: 100,
   },
   description: {
     fontSize: 16,
